087710cb69cc30532a14a1aa10f0f2f00fcadd98
shiva <shivamotog62@gmail.com>
2024-07-05 02:09:16 +0530

    3c

--- src/components/Farmer/index.jsx
+++ src/components/Farmer/index.jsx
@@ -1,9 +1,78 @@
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
 import "./index.css";
 
 const Farmer = () => {
+  const [weatherData, modifyWeatherData] = useState({});
+  const [fetchStatus, changeFetchStatus] = useState("loading");
+  const [place, changePlace] = useState("Hyderabad");
+  const [date, changeDate] = useState(format(new Date(), "yyyy-MM-dd"));
+
+  useEffect(() => {
+    const baseUrl = `https://api.weatherapi.com/v1/future.json?key=2a4b4962a62b47f5b47162651240407&q=${place}&dt=${date}`;
+    const fetchWeatherData = async () => {
+      const response = await fetch(baseUrl);
+      if (response.ok === true) {
+        changeFetchStatus("success");
+        modifyWeatherData(await response.json());
+      } else {
+        changeFetchStatus("failed");
+        modifyWeatherData(await response.json());
+      }
+    };
+    fetchWeatherData();
+    console.log(place, date);
+  }, []);
+
+  const onPlaceChange = (e) => {
+    changePlace(e.target.value);
+  };
+
+  const onDateChange = (e) => {
+    changeDate(e.target.value);
+  };
+
+  const onBtnClick = () => {
+    console.log(place, date);
+  };
+
+  const renderInputElements = () => {
+    return (
+      <div className="inputsDiv">
+        <label className="inputLabel" htmlFor="ipBox">
+          Place:
+        </label>
+        <input
+          id="ipBox"
+          className="inputBox"
+          value={place}
+          placeholder="Enter any Place"
+          onChange={onPlaceChange}
+          type="text"
+        />
+
+        <label className="inputLabel" htmlFor="ipDate">
+          Date:
+        </label>
+        <input
+          onChange={onDateChange}
+          id="ipDate"
+          className="inputBox dateBox"
+          type="date"
+          value={date}
+        />
+
+        <button onClick={onBtnClick} type="button" className="forcastBtn">
+          Get Forcast
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="farmerComponent">
-      <h1>This is from Farmer Component</h1>
+      <h1 className="farmersHeading">Welcome to Farmers Corner </h1>
+      {renderInputElements()}
     </div>
   );
 };
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,16 +4,27 @@ import "./index.css";
 const Home = () => {
   return (
     <div className="homeComponent">
-      <h1>Weather to Work Out</h1>
+      <h2 className="greeting">Hello.. Welcome To</h2>
+      <h1 className="heading"> Weather to Work Out </h1>
+      <h3 className="ima">I'm</h3>
       <div className="mainMenu">
         <Link to="/farmer">
-          <h2 className="menuItem">Farmer</h2>
+          <div className="menuItem">
+            <img className="menuImg" src="../../../images/farmer.jpg" />
+            <p className="des">Farmer</p>
+          </div>
         </Link>
         <Link to="/traveller">
-          <h2 className="menuItem">Traveller</h2>
+          <div className="menuItem">
+            <img className="menuImg" src="../../../images/traveller.jpg" />
+            <p className="des">Traveller</p>
+          </div>
         </Link>
         <Link to="/eventplanner">
-          <h2 className="menuItem">Event Planner</h2>
+          <div className="menuItem">
+            <img className="menuImg" src="../../../images/event.jpg" />
+            <p className="des">Planning an Event</p>
+          </div>
         </Link>
       </div>
     </div>
